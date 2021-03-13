ee2f7a67b7dbcee4346b774d53b7cc89a6a78e9b
Paul <pfvatterott@gmail.com>
2021-03-13 09:12:17 -0700

    no longer possible to submit an empty burger_name

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -8,23 +8,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (createBurgerBtn) {
         createBurgerBtn.addEventListener('submit', (e) => {
             e.preventDefault();
-            console.log("button works")
-            
-            const newBurger = {
-                burger_name: document.getElementById('create_burger').value.trim(),
-            };
-
-            fetch('/api/burgers', {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(newBurger),
-            }).then(() => {
-                document.getElementById('create_burger').value = '';
-                location.reload();
-            });
+            if (document.getElementById('create_burger').value.trim() === "") {
+                return
+            } else {
+                const newBurger = {
+                    burger_name: document.getElementById('create_burger').value.trim(),
+                };
+    
+                fetch('/api/burgers', {
+                    method: 'POST',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(newBurger),
+                }).then(() => {
+                    document.getElementById('create_burger').value = '';
+                    location.reload();
+                });
+            } 
         })
     }
 
